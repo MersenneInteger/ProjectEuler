6e544e0440d9e8935e40bef5a8ea275f513589f3
nero <aUtomata2.71828@openmailbox.org>
2015-11-03 21:28:24 -0600

    problem 1

--- multiplesOf3and5.go
+++ multiplesOf3and5.go
@@ -2,13 +2,14 @@ package main
 import "fmt"
 func main(){
   
-  var sum int = 0
+  sum := 0
   i := 0
 
   for i < 1000 {
-	if i %3==0 || i % 5==0 { sum += i }
-	i++
-	}
+     if i %3==0 || i % 5==0 
+        { sum += i }
+     i++
+  }
 	
-	fmt.Println(sum)
+  fmt.Println(sum)
 }
