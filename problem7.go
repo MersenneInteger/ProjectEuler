cffaa413ee0972a4407557cf2f8438e2bfceef4e
MersenneInteger <aUtomata2.71828@openmailbox.org>
2016-08-02 13:15:51 -0500

    10001st prime

---
+++ problem7.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main(){
+
+    var target, count int64 = 0,2
+    
+    for count = 2; target < 10002; count++ {
+
+        if (isPrime(count)) == true { target++ }       
+        if target == 10001 && ((isPrime(count)) == true) {
+            fmt.Print(count)
+            break
+        } 
+    }
+}
+func isPrime(n int64) bool {
+
+    var i int64 = 2; 
+    for i = 2; i <= (n/2); i++ {
+        if n % i == 0 { return false }
+    }
+    return true
+}
